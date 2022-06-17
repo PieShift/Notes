0c7fd721a6690ede114d541b23b6c7b6637d8053
PieShift <106903584+PieShift@users.noreply.github.com>
2022-06-17 17:10:07 +0300

    Update T1.js

--- Test_1/T1.js
+++ Test_1/T1.js
@@ -6,30 +6,31 @@ async function testTaxiResult() {
     /// Запуск браузера
     const browser = await puppeteer.launch({headless: false, slowMo: 100});
 
-    console.log('Создание новой вкладки в браузере');
+    /// Создание новой вкладки в браузере
     const page = await browser.newPage();
 
-    console.log('Переход по ссылке');
+    /// Переход по ссылке
     await page.goto(URL_TEST);
 
-    console.log('Шаг 1: ввод часов и минут');
+    /// Шаг 1: ввод часов и минут
     const hoursInput = await page.$('#form-input-hour');
     await hoursInput.type('08');
 
     const minutesInput = await page.$('#form-input-minute');
     await minutesInput.type('00');
         
-    console.log('Шаг 2: заполнение поля Откуда');
+    /// Шаг 2: заполнение поля Откуда
     const fromInput = await page.$('#form-input-from');
     await fromInput.type('Усачева, 3');
 
-    console.log('Шаг 3: заполнение поля Куда');
+    /// Шаг 3: заполнение поля Куда
     const toInput = await page.$('#form-input-to');
     await toInput.type('Комсомольский проспект, 18');
-
+    
+    /// Опционально - скриншот
     await page.screenshot({path: 'testTaxiResult.png'});
 
-    console.log('Закрытие браузера');
+    /// Закрытие браузера
     await browser.close();
 }
 
